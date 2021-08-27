1633e9fc57c679f99940296d57ad4cb16ad1dd6e
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-27 22:53:24 +0300

    Summary

--- src/MyShop/urls.py
+++ src/MyShop/urls.py
@@ -1,4 +1,4 @@
-"""DjangoOscarProject URL Configuration
+"""MyShop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.apps import apps
-from django.urls import include, path
 from django.contrib import admin
+######## СОГЛАСНО ДОКУМЕНТАЦИИ ########
+from django.urls import path, include
 
-from django.conf.urls.static import static
-from django.conf import settings
+from django.apps import apps
 
 
 urlpatterns = [
@@ -31,6 +30,4 @@ urlpatterns = [
 
     path('', include(apps.get_app_config('oscar').urls[0])),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#######################################
--- src/MyShop/wsgi.py
+++ src/MyShop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoOscarProject project.
+WSGI config for MyShop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoOscarProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyShop.settings')
 
 application = get_wsgi_application()
