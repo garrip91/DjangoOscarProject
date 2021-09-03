f8eda54e109607d5d0fbb684f84207a25202b7bf
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-09-03 16:53:55 +0300

    Summary

--- src/MyShop/settings.py
+++ src/MyShop/settings.py
@@ -131,7 +131,7 @@ ROOT_URLCONF = 'MyShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -205,3 +205,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
